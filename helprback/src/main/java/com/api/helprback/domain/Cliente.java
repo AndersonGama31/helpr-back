ab3ea556e780833476602d7d42bef45b2c7ce420
AndersonGama31 <andersongama31@gmail.com>
2022-08-16 13:18:06 -0300

    Desenvolvido o CRUD de cliente no helpr-back

--- helprback/src/main/java/com/api/helprback/domain/Cliente.java
+++ helprback/src/main/java/com/api/helprback/domain/Cliente.java
@@ -1,13 +1,20 @@
 package com.api.helprback.domain;
 
+import com.api.helprback.domain.dtos.ClienteDTO;
+import com.api.helprback.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 public class Cliente extends Pessoa{
 
     private static final long serialVersionUID = 1L;
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
@@ -19,6 +26,16 @@ public class Cliente extends Pessoa{
         super(id, nome, cpf, email, senha);
     }
 
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
     public List<Chamado> getChamados() {
         return chamados;
     }
